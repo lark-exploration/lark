1487b4d613aa34bf761392a7498c74cffca1a6e0
Jonathan Turner <jonathan.d.turner@gmail.com>
2018-09-13 11:30:11 +1200

    Start eval system

---
+++ src/eval.rs
@@ -0,0 +1,130 @@
+use crate::ir::{builtin_type, BuiltinFn, Command, Context, DefId, Definition, Function, Struct};
+use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Clone, Debug)]
+pub enum Value {
+    Void,
+    I32(i32),
+    Str(String),
+    Reference(usize), // a reference into the value stack
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Value::I32(i) => i.to_string(),
+                Value::Str(s) => s.clone(),
+                Value::Reference(r) => format!("reference to {}", r),
+                Value::Void => "<void>".into(),
+            }
+        )
+    }
+}
+
+pub struct Eval {
+    pub stack: Vec<Value>,
+}
+
+impl Eval {
+    pub fn new() -> Eval {
+        Eval { stack: vec![] }
+    }
+
+    pub fn eval_block(
+        &mut self,
+        c: &Context,
+        vars: &mut HashMap<usize, usize>,
+        commands: &Vec<Command>,
+    ) -> Value {
+        for command in commands {
+            println!("{:?}", command);
+            match command {
+                Command::ConstInt(i) => self.stack.push(Value::I32(*i)),
+                Command::ConstString(s) => self.stack.push(Value::Str(s.clone())),
+                Command::Add => {
+                    let rhs = self.stack.pop().unwrap();
+                    let lhs = self.stack.pop().unwrap();
+                    match (lhs, rhs) {
+                        (Value::I32(l), Value::I32(r)) => {
+                            self.stack.push(Value::I32(l + r));
+                        }
+                        _ => unimplemented!("Unsupported add of non-integers"),
+                    }
+                }
+                Command::Sub => {
+                    let rhs = self.stack.pop().unwrap();
+                    let lhs = self.stack.pop().unwrap();
+                    match (lhs, rhs) {
+                        (Value::I32(l), Value::I32(r)) => {
+                            self.stack.push(Value::I32(l - r));
+                        }
+                        _ => unimplemented!("Unsupported subtract of non-integers"),
+                    }
+                }
+                Command::DebugPrint => {
+                    let arg = self.stack.pop().unwrap();
+
+                    println!("{}", arg);
+                }
+                Command::VarUse(var_id) => {
+                    println!("Using: {}", var_id);
+                    println!("{:#?}", self.stack);
+                    println!("{:#?}", vars);
+                    let stack_pos = vars[var_id];
+                    let var_use = self.stack[stack_pos].clone();
+                    self.stack.push(var_use);
+                }
+                Command::VarDeclWithInit(var_id) => {
+                    vars.insert(*var_id, self.stack.len() - 1);
+                }
+                Command::Call(def_id) => match &c.definitions[*def_id] {
+                    Definition::Fn(ref f) => {
+                        let result = self.eval_fn(c, f);
+                        self.stack.push(result);
+                    }
+                    x => unimplemented!("Call of a non-function: {:#?}", x),
+                },
+                Command::ReturnLastStackValue => {
+                    let result = self.stack.pop().unwrap();
+                    return result;
+                }
+                _ => unimplemented!("Incomplete eval of commands in eval_fn"),
+            }
+        }
+
+        Value::Void
+    }
+
+    pub fn eval_fn(&mut self, c: &Context, f: &Function) -> Value {
+        let mut vars = HashMap::new();
+
+        //Rather than popping the values only to push them back on so the
+        //function body can see it, let's instead capture where the arguments are in the stack
+        //by setting up the function's variables ahead of processing the body
+        let mut offset = 0;
+        let param_len = f.params.len();
+        for param in &f.params {
+            vars.insert(param.var_id, self.stack.len() - param_len + offset);
+            offset += 1;
+        }
+
+        self.eval_block(c, &mut vars, &f.body)
+    }
+
+    pub fn eval(&mut self, c: &Context) {
+        for definition in &c.definitions {
+            match definition {
+                Definition::Fn(f) => {
+                    if f.name == "main" {
+                        self.eval_fn(c, f);
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,21 @@
 #![feature(crate_visibility_modifier)]
 #![feature(nll)]
 #![allow(unused)]
+#![feature(crate_visibility_modifier)]
 
 mod codegen;
+mod eval;
 mod ir;
 mod parser;
 
 use crate::codegen::{codegen, RustFile};
+use crate::eval::Eval;
 use crate::ir::{builtin_type, Command, Context, Definition, Function, Struct};
 
 fn main() {
     let mut c = Context::new();
 
-    let borrow_str_id = c.add_definition(Definition::Borrow(builtin_type::STRING));
+    //let borrow_str_id = c.add_definition(Definition::Borrow(builtin_type::STRING));
 
     let mut bob = Function::new("bob".into(), builtin_type::I32)
         .param("x".into(), builtin_type::I32)
@@ -30,12 +33,15 @@ fn main() {
 
     let bob_def_id = c.add_definition(Definition::Fn(bob));
 
+    /*
     let person = Struct::new("Person".into())
         .field("height".into(), builtin_type::I32)
         .field("id".into(), builtin_type::I32);
 
     let person_def_id = c.add_definition(Definition::Struct(person));
+    */
 
+    /*
     let mut greet =
         Function::new("greet".into(), builtin_type::VOID).param("name".into(), borrow_str_id);
 
@@ -43,32 +49,46 @@ fn main() {
     greet.body.push(Command::DebugPrint);
 
     let greet_def_id = c.add_definition(Definition::Fn(greet));
+    */
 
     let mut m = Function::new("main".into(), builtin_type::VOID);
     m.body.push(Command::ConstInt(11));
     m.body.push(Command::ConstInt(8));
     m.body.push(Command::Call(bob_def_id));
-    m.body.push(Command::ConstString("Hello, world {}".into()));
-    m.body.push(Command::Call(101)); //built-in string interpolation
     m.body.push(Command::DebugPrint);
+    //m.body.push(Command::ConstString("Hello, world {}".into()));
+    //m.body.push(Command::Call(101)); //built-in string interpolation
+    //m.body.push(Command::DebugPrint);
+
+    /*
     m.body.push(Command::ConstString("Samwell".into()));
     m.body.push(Command::Call(greet_def_id));
     m.body.push(Command::DebugPrint);
     m.body.push(Command::ConstInt(17));
     m.body.push(Command::ConstInt(18));
     m.body.push(Command::CreateStruct(person_def_id));
+
     m.body.push(Command::VarDeclWithInit(0));
     m.body.push(Command::VarUse(0));
     m.body.push(Command::DebugPrint);
     m.body.push(Command::VarUse(0));
     m.body.push(Command::Dot("id".into()));
     m.body.push(Command::DebugPrint);
-
+    */
     c.definitions.push(Definition::Fn(m));
+
+    // let mut m = Function::new("main".into(), builtin_type::VOID);
+    // m.body.push(Command::ConstInt(11));
+    // m.body.push(Command::ConstInt(8));
+    // m.body.push(Command::Add);
+    // m.body.push(Command::DebugPrint);
+    // c.definitions.push(Definition::Fn(m));
 
     let mut rust = RustFile::new();
 
     codegen(&mut rust, &c);
-
     println!("{}", rust.render());
+
+    let mut eval = Eval::new();
+    eval.eval(&c);
 }
