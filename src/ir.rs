64c1d6855ed8720b93bd4552d11fbf67f74539c1
Jonathan Turner <jonathan.d.turner@gmail.com>
2018-09-24 11:19:46 +1200

    WIP: Move to MIR-style IR

--- src/ir.rs
+++ src/ir.rs
@@ -2,6 +2,201 @@ pub type DefId = usize;
 pub type VarId = usize;
 
 #[derive(Debug)]
+pub struct SourceInfo;
+
+//Lark MIR representation of a single function
+#[derive(Debug)]
+pub struct Function {
+    basic_blocks: Vec<BasicBlock>,
+
+    //First local = return value pointer
+    //Followed by arg_count parameters to the function
+    //Followed by user defined variables and temporaries
+    local_decls: Vec<LocalDecl>,
+
+    arg_count: usize,
+}
+
+impl Function {
+    pub fn new(return_ty: DefId, args: Vec<LocalDecl>) -> Function {
+        let mut local_decls = vec![LocalDecl::new_return_place(return_ty)];
+        local_decls.append(&mut args);
+        let arg_count = local_decls.len();
+
+        Function {
+            basic_blocks: vec![],
+            local_decls,
+            arg_count,
+        }
+    }
+
+    pub fn new_temp(&mut self, ty: DefId) -> VarId {
+        self.local_decls.push(LocalDecl::new_temp(ty));
+        self.local_decls.len() - 1
+    }
+
+    pub fn push_block(&mut self, block: BasicBlock) {
+        self.basic_blocks.push(block);
+    }
+}
+
+#[derive(Debug)]
+pub struct BasicBlock {
+    pub statements: Vec<Statement>,
+    pub terminator: Option<Terminator>,
+}
+
+impl BasicBlock {
+    pub fn new() -> BasicBlock {
+        BasicBlock {
+            statements: vec![],
+            terminator: None,
+        }
+    }
+
+    pub fn push_stmt(&mut self, kind: StatementKind) {
+        self.statements.push(Statement {
+            source_info: SourceInfo,
+            kind,
+        });
+    }
+
+    pub fn terminate(&mut self, terminator_kind: TerminatorKind) {
+        self.terminator = Some(Terminator {
+            source_info: SourceInfo,
+            kind: terminator_kind,
+        });
+    }
+}
+
+#[derive(Debug)]
+pub struct Statement {
+    pub source_info: SourceInfo,
+    pub kind: StatementKind,
+}
+
+#[derive(Debug)]
+pub enum StatementKind {
+    Assign(Place, Rvalue),
+    DebugPrint(Rvalue),
+}
+
+#[derive(Debug)]
+pub struct Terminator {
+    pub source_info: SourceInfo,
+    pub kind: TerminatorKind,
+}
+
+#[derive(Debug)]
+pub enum TerminatorKind {
+    Return,
+}
+
+#[derive(Debug)]
+pub enum Place {
+    Local(VarId),
+    Static(DefId),
+}
+
+#[derive(Debug)]
+pub enum Rvalue {
+    Use(Operand),
+    BinaryOp(BinOp, VarId, VarId),
+}
+
+#[derive(Debug)]
+pub enum Operand {
+    Copy(Place),
+    Move(Place),
+    //FIXME: MIR has this as a Terminator, presumably because stack can unwind
+    Call(DefId, Vec<Operand>),
+    //FIXME: Move to Box<Constant>
+    ConstantInt(i32),
+    ConstantString(String),
+}
+
+#[derive(Debug)]
+pub enum BinOp {
+    Add,
+    Sub,
+}
+
+#[derive(Debug)]
+pub struct LocalDecl {
+    pub ty: DefId,
+    pub name: Option<String>,
+}
+
+impl LocalDecl {
+    pub fn new_return_place(return_ty: DefId) -> LocalDecl {
+        LocalDecl {
+            ty: return_ty,
+            name: None,
+        }
+    }
+
+    pub fn new_temp(ty: DefId) -> LocalDecl {
+        LocalDecl { ty, name: None }
+    }
+
+    pub fn new(ty: DefId, name: Option<String>) -> LocalDecl {
+        LocalDecl { ty, name }
+    }
+}
+
+pub mod builtin_type {
+    #[allow(unused)]
+    pub const UNKNOWN: usize = 0;
+    pub const VOID: usize = 1;
+    pub const I32: usize = 2;
+    pub const STRING: usize = 3;
+    pub const ERROR: usize = 100;
+}
+
+#[derive(Debug)]
+pub enum BuiltinFn {
+    StringInterpolate,
+}
+
+#[derive(Debug)]
+pub enum Definition {
+    Builtin,
+    BuiltinFn(BuiltinFn),
+    Fn(Function),
+}
+
+pub struct Context {
+    pub definitions: Vec<Definition>,
+}
+
+impl Context {
+    pub fn new() -> Context {
+        let mut definitions = vec![];
+
+        for _ in 0..(builtin_type::ERROR + 1) {
+            definitions.push(Definition::Builtin); // UNKNOWN
+        }
+
+        definitions.push(Definition::BuiltinFn(BuiltinFn::StringInterpolate));
+
+        Context { definitions }
+    }
+
+    pub fn add_definition(&mut self, def: Definition) -> usize {
+        self.definitions.push(def);
+        self.definitions.len() - 1
+    }
+}
+
+/*
+use indexed_vec::{newtype_index, IndexVec};
+
+pub type DefId = usize;
+pub type VarId = usize;
+
+struct BasicBlock;
+
+#[derive(Debug)]
 pub struct Variable {
     pub ty: DefId,
     pub name: String,
@@ -127,3 +322,4 @@ impl Context {
         self.definitions.len() - 1
     }
 }
+*/
--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,86 @@ mod ty;
 
 use crate::codegen::{codegen, RustFile};
 use crate::eval::Eval;
-use crate::ir::{builtin_type, Command, Context, Definition, Function, Struct};
+use crate::ir::{
+    builtin_type, BasicBlock, BinOp, Context, Definition, Function, LocalDecl, Operand, Place,
+    Rvalue, StatementKind, TerminatorKind,
+};
 
 fn main() {
     let mut c = Context::new();
 
+    let mut bob = Function::new(
+        builtin_type::I32,
+        vec![
+            LocalDecl::new(builtin_type::I32, Some("x".into())),
+            LocalDecl::new(builtin_type::I32, Some("y".into())),
+        ],
+    );
+
+    let bob_tmp = bob.new_temp(builtin_type::I32);
+
+    let mut bb1 = BasicBlock::new();
+
+    bb1.push_stmt(StatementKind::Assign(
+        Place::Local(bob_tmp),
+        Rvalue::BinaryOp(BinOp::Sub, 1, 2),
+    ));
+    bb1.push_stmt(StatementKind::Assign(
+        Place::Local(0),
+        Rvalue::Use(Operand::Move(Place::Local(bob_tmp))),
+    ));
+
+    bb1.terminate(TerminatorKind::Return);
+
+    bob.push_block(bb1);
+
+    let bob_def_id = c.add_definition(Definition::Fn(bob));
+
+    // bob.body.push(Command::VarUse(0));
+    // bob.body.push(Command::VarUse(1));
+    // bob.body.push(Command::Sub);
+    // bob.body.push(Command::VarDeclWithInit(2));
+    // bob.body.push(Command::VarUse(2));
+    // bob.body.push(Command::ReturnLastStackValue);
+
+    let mut m = Function::new(builtin_type::VOID, vec![]);
+    let call_result_tmp = m.new_temp(builtin_type::I32);
+    let interp_result_tmp = m.new_temp(builtin_type::STRING);
+
+    let mut bb2 = BasicBlock::new();
+
+    bb2.push_stmt(StatementKind::Assign(
+        Place::Local(call_result_tmp),
+        Rvalue::Use(Operand::Call(
+            bob_def_id,
+            vec![Operand::ConstantInt(11), Operand::ConstantInt(8)],
+        )),
+    ));
+
+    bb2.push_stmt(StatementKind::Assign(
+        Place::Local(interp_result_tmp),
+        Rvalue::Use(Operand::Call(
+            101, /*builtin string interp*/
+            vec![
+                Operand::ConstantString("Hello, world {}".into()),
+                Operand::Move(Place::Local(call_result_tmp)),
+            ],
+        )),
+    ));
+
+    bb2.push_stmt(StatementKind::DebugPrint(Rvalue::Use(Operand::Move(
+        Place::Local(interp_result_tmp),
+    ))));
+
+    // let mut m = Function::new("main".into(), builtin_type::VOID);
+    // m.body.push(Command::ConstInt(11));
+    // m.body.push(Command::ConstInt(8));
+    // m.body.push(Command::Call(bob_def_id));
+    // m.body.push(Command::ConstString("Hello, world {}".into()));
+    // m.body.push(Command::Call(101)); //built-in string interpolation
+    // m.body.push(Command::DebugPrint);
+
+    /*
     let mut bob = Function::new("bob".into(), builtin_type::I32)
         .param("x".into(), builtin_type::I32)
         .param("y".into(), builtin_type::I32);
@@ -69,6 +144,7 @@ fn main() {
     m.body.push(Command::DebugPrint);
 
     c.definitions.push(Definition::Fn(m));
+    */
 
     let mut rust = RustFile::new();
 
