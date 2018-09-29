9a6ef51cac48bab669a9b777b72ed5bbfbdf6547
Jonathan Turner <jonathan.d.turner@gmail.com>
2018-09-29 15:15:32 +1200

    Initial commit of language server support

--- src/main.rs
+++ src/main.rs
@@ -26,16 +26,62 @@ mod parser;
 mod ty;
 mod typeck;
 
+use std::io::prelude::Read;
+use std::{env, io};
+
 use crate::codegen::{codegen, RustFile};
 use crate::eval::eval_context;
 use crate::ir::{
     builtin_type, BasicBlock, BinOp, Context, Definition, Function, LocalDecl, Operand, Place,
     Rvalue, StatementKind, Struct, TerminatorKind,
 };
+use serde_derive::{Deserialize, Serialize};
 
 use crate::ty::intern::TyInterners;
 
-fn main() {
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "method")]
+#[allow(non_camel_case_types)]
+enum LSCommand {
+    initialize {
+        id: usize,
+        params: languageserver_types::InitializeParams,
+    },
+}
+
+fn build(_filename: &str) {}
+
+fn run(_filename: &str) {}
+
+fn repl() {}
+
+fn ide() {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            //eprintln!("{} bytes read", n);
+            //eprintln!("{}", input);
+            let content_length_items: Vec<&str> = input.split(' ').collect();
+            if content_length_items[0] == "Content-Length:" {
+                let num_bytes: usize = content_length_items[1].trim().parse().unwrap();
+                let mut buffer = vec![0u8; num_bytes + 2];
+                let read_result = io::stdin().read(&mut buffer);
+
+                eprintln!("read in {:?}", read_result);
+
+                let buffer_string = String::from_utf8(buffer).unwrap();
+                eprintln!("command: {}", buffer_string);
+
+                let command = serde_json::from_str::<LSCommand>(&buffer_string);
+
+                eprintln!("Command: {:#?}", command);
+            }
+        }
+        Err(error) => eprintln!("error: {}", error),
+    }
+}
+
+fn internaltest() {
     let mut c = Context::new();
 
     let i32_ty = c.simple_type_for_def_id(builtin_type::I32);
@@ -127,8 +173,25 @@ fn main() {
     codegen(&mut rust, &c);
     println!("{}", rust.render());
 
-    //let mut eval = Eval::new();
-    //eval.eval(&c);
-
     eval_context(&c, main_def_id);
+}
+
+fn main() {
+    let mut args = std::env::args();
+
+    match (args.next(), args.next(), args.next()) {
+        (_, Some(ref cmd), Some(ref x)) if cmd == "build" => build(x),
+        (_, Some(ref cmd), Some(ref x)) if cmd == "run" => run(x),
+        (_, Some(ref cmd), None) if cmd == "repl" => repl(),
+        (_, Some(ref cmd), None) if cmd == "ide" => ide(),
+        (_, Some(ref cmd), None) if cmd == "internaltest" => internaltest(),
+        _ => {
+            println!("Usage:");
+            println!("  lark build <file> - compiles the given file");
+            println!("  lark run <file>   - runs the given file");
+            println!("  lark repl         - REPL/interactive mode");
+            println!("  lark ide          - run the Lark languge server/IDE support");
+            println!("  lark internaltest - run some internal tests");
+        }
+    }
 }
