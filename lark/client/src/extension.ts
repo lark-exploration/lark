4eb183dd19c339873d4941e79c84385a563a5d05
Jonathan Turner <jonathan.d.turner@gmail.com>
2018-10-01 07:06:38 +1300

    Add support for document opening and hover

---
+++ lark/client/src/extension.ts
@@ -0,0 +1,56 @@
+'use strict';
+
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+} from 'vscode-languageclient';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+
+	let serverPath = context.asAbsolutePath(path.join("..", "target", "debug", "larkc.exe"));
+
+	let serverOptions: ServerOptions = {
+		run: { command: serverPath, args: ["ide"], options: { env: { "RUST_BACKTRACE": 1 } } },
+		debug: {
+			command: serverPath,
+			args: ["ide"],
+			options: { env: { "RUST_BACKTRACE": 1 } }
+		}
+	};
+
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'lark' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/*.lark')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'LarkLSP',
+		'Lark IDE support',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
