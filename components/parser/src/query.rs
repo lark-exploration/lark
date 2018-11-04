231d2e7acd37347ae6fda481b7b0e4be41694598
Niko Matsakis <niko@alum.mit.edu>
2018-11-04 05:18:29 -0500

    merge the `files` and `reader` code
    We will want them to share a privacy boundary.

--- components/parser/src/query.rs
+++ components/parser/src/query.rs
@@ -2,6 +2,7 @@ pub mod files;
 pub mod parser_state;
 pub mod reader;
 
-pub use self::files::SourceFiles;
 pub use self::parser_state::{HasParserState, InputText, ParserState};
-pub use self::reader::{add_file, initialize_reader, Files, Paths, ReaderDatabase, Source};
+pub use self::reader::{
+    add_file, initialize_reader, Files, Paths, ReaderDatabase, Source, SourceFiles,
+};
