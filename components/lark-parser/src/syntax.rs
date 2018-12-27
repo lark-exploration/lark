038826da2bd5828ff3672aa74dd3fa5f7dbdb39a
Yehuda Katz <wycats@gmail.com>
2018-12-27 09:28:16 +0000

    Extract expressions into their own file

--- components/lark-parser/src/syntax.rs
+++ components/lark-parser/src/syntax.rs
@@ -15,6 +15,7 @@ pub mod matched;
 pub mod sigil;
 pub mod skip_newline;
 pub mod type_reference;
+pub mod expression;
 
 pub trait Syntax<'parse>: DebugWith {
     /// The value that is produced (often, but not always, `Self`) by the
